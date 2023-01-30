7f41a4e4f06d3bd548fe873d4c7ff175fe1e5b73
sorabotiscrazy <122054672+sorabotiscrazy@users.noreply.github.com>
2023-01-30 19:10:23 +0000

    Update snipe.py

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -447,11 +447,8 @@ class MsgSource(menus.ListPageSource):
 
     async def format_page(self, menu, msg):
         emb = self.template_emb.copy()
-        emb.title = f"Message Contents (Sent <t:{msg.created_at}:R>)"
         emb.description = msg.content
         emb.set_author(name=f"{msg.author} ({msg.author.id})", icon_url=msg.author.avatar_url)
-        emb.add_field(name="Channel", value=f"<#{msg.channel.id}>")
-        emb.add_field(name="Deleted At", value=f"<t:{msg.deleted_at}:R>")
         emb.set_footer(
             text=f"Sniped at {menu.ctx.guild} | Page {menu.current_page+1}/{self._max_pages}",
             icon_url=menu.ctx.guild.icon_url,
