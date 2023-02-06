66a223715ba48a5d8326e706a684115ff040f626
sorabotiscrazy <122054672+sorabotiscrazy@users.noreply.github.com>
2023-02-06 19:58:20 +0000

    Update snipe.py

--- snipe/snipe.py
+++ snipe/snipe.py
@@ -449,10 +449,6 @@ class MsgSource(menus.ListPageSource):
         emb.title = f""
         emb.description = msg.content
         emb.set_author(name=f"{msg.author} ({msg.author.id})", icon_url=msg.author.avatar_url)
-        emb.set_footer(
-            text=f"Sniped by {ctx.self.author}",
-            icon_url=menu.ctx.guild.icon_url,
-        )
 
         return emb
 
